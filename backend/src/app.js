f56ca98807e52e1721269ec0e1f20385f1fdfe50
Nitish Thakur <tnitish440@gmail.com>
2026-07-19 00:39:06 +0530

    domain modified in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://sportify-clone-chi-sable.vercel.app/",
+      "https://sportify-clone-chi-sable.vercel.app",
     ],
     credentials: true,
   }),
